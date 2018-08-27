1cbd86d75d3167458ebd96283a9f8c65e476c354
HaoDaWang <1306036576@qq.com>
2018-08-27 20:42:21 +0800

    init

---
+++ src/components/monaco-md-editor.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import "../styles/App.css";
+
+class App extends React.Component {
+  render(): JSX.Element {
+    return <div>Hello</div>;
+  }
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "components/monaco-md-editor";
+import "./styles/main.css";
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
