dcec8d2d3499c6edde9714e3614ed5695edc6523
HaoDaWang <1306036576@qq.com>
2018-08-28 22:06:21 +0800

    Add size  util

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,3 +1,14 @@
-export function Size(size: string | undefined = "100%"): string {
-  return size.indexOf("%") ? size : `${size}px`;
+import { Style } from "../components/common/style-props";
+
+export function Size(style: Style | undefined): Style {
+  const _style = style || { height: "100%", width: "100%" };
+  const defaultStyle = (size: string | undefined): string => {
+    let _size = size || "100%";
+    return !/^\d+$/.test(_size) ? _size : `${_size}px`;
+  };
+
+  return {
+    width: defaultStyle(_style.width),
+    height: defaultStyle(_style.height)
+  };
 }
