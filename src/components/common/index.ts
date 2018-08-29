9c246e6241306c912bcd6338772772054e1b835f
HaoDaWang <1306036576@qq.com>
2018-08-29 23:46:46 +0800

    Export props

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,5 @@
 export * from "./editor-option";
 export * from "./editor-props";
-export * from './style-props';
+export * from "./style-props";
+export * from "./content-props";
+export * from "./icon";
