db4e9ec3e033e933070e9f99fdc5f968cbf9e52c
HaoDaWang <1306036576@qq.com>
2018-08-30 16:06:55 +0800

    Add  header click logic

--- src/components/header/header-pop/header-pop.tsx
+++ src/components/header/header-pop/header-pop.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 
 import { editorStore } from "../../../build-stores";
 import "../../../styles/header-pop.css";
+import { attachSymbol, createSymbol } from "../../../utils";
 
 @observer
 export class HeaderPop extends React.Component {
@@ -19,17 +20,21 @@ export class HeaderPop extends React.Component {
     }
   }
 
-  handleClick(): void {
+  handleClick(headerIndex: number): void {
     editorStore.isView = !editorStore.isView;
+    editorStore.content = attachSymbol(
+      createSymbol("#", headerIndex, true, "end"),
+      ""
+    );
   }
 
   render(): JSX.Element {
     const headerPop = editorStore.isView ? (
-      <div className="PopBody">
+      <div className="PopBody" onClick={(): void => console.info("hello")}>
         {this.headers.map((header, index) => (
           <div
             key={index}
-            onClick={() => this.handleClick()}
+            onClick={() => this.handleClick(index + 1)}
             className="HeaderPopItem"
             style={{
               fontSize: this.headerFontSize - index * this.fontSizeIncrement,
